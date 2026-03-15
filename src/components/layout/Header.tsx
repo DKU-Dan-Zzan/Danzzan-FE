c15e68b3cdd8cf3f9e4faf0f7d6207d9e8746fac
juhee0223 <pjuhee23@dankook.ac.kr>
2026-03-15 19:12:19 +0900

    [DANZ-204] chore: 헤더 로고 크기 축소

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
         <img
           src="/DAN-ZZAN.png"
           alt="DAN-ZZAN"
-          className="pointer-events-none absolute top-[75%] left-1/2 h-[68px] w-[300px] -translate-x-1/2 -translate-y-1/2 object-cover object-[50%_58%] select-none"
+          className="pointer-events-none absolute top-[75%] left-1/2 h-[61px] w-[270px] -translate-x-1/2 -translate-y-1/2 object-cover object-[50%_58%] select-none"
           draggable={false}
         />
 
