eb9e4691e2fa37ad53a1e6ac80ddaa9b36ace837
juhee0223 <pjuhee23@dankook.ac.kr>
2026-03-22 01:40:51 +0900

    [DANZ-228] chore: 브랜딩 에셋 및 헤더 로고 정렬 보정

--- src/components/layout/AppHeaderLogo.tsx
+++ src/components/layout/AppHeaderLogo.tsx
@@ -3,7 +3,7 @@ type AppHeaderLogoProps = {
 };
 
 const BASE_CLASS_NAME =
-  "pointer-events-none absolute top-[75%] left-1/2 h-[61px] w-[270px] -translate-x-1/2 -translate-y-1/2 object-cover object-[50%_58%] select-none";
+  "pointer-events-none absolute top-1/2 left-1/2 h-[50px] w-[220px] -translate-x-1/2 -translate-y-1/2 object-contain object-center select-none";
 
 export function AppHeaderLogo({ className }: AppHeaderLogoProps) {
   return (
